1c495521f14497b98ebab60bf8d256bcbbd92734
Konstantin Grigorev <info@a6raywa1cher.com>
2023-05-02 23:44:43 +0300

    finish first block of tasks

--- libs/audio.py
+++ libs/audio.py
@@ -1,7 +1,8 @@
+from scipy.io import loadmat
 from scipy.io.wavfile import read, write
 import numpy as np
 
-from libs.path import get_resources_path, get_output_resources_path
+from libs.path import get_resources_path, get_output_resources_path, get_ext
 
 
 def get_quantization_bit_depth(data):
@@ -44,10 +45,37 @@ def denormalize_data(data, max_value):
     return (clipped_data * max_value).astype(new_type)
 
 
-def read_audio(file_name):
-    sampling_frequency, data = read(get_resources_path(file_name))
+def load_wav_file(filename):
+    sampling_frequency, data = read(get_resources_path(filename))
     normalized_data, max_value = normalize_data(data)
     return sampling_frequency, normalized_data, max_value
+
+
+def load_mat_file_raw(filename):
+    return loadmat(filename, squeeze_me=True, struct_as_record=False)
+
+
+def load_mat_file(filename, fields):
+    mat = load_mat_file_raw(filename)
+    out = dict()
+    max_value = None
+    for field in fields:
+        outer_data = mat[field]
+        if not hasattr(outer_data, '_fieldnames'):
+            raise ValueError(f"can't load {field} from mat file {filename}: no _fieldnames")
+        for name in outer_data._fieldnames:
+            inner_data = getattr(outer_data, name)
+            normalized_data, max_value_inner = normalize_data(inner_data)
+            max_value = max_value or max_value_inner
+            if max_value != max_value_inner:
+                raise ValueError(f"can't load {field} from mat file {filename}: "
+                                 f"invalid max_value {max_value_inner}, expected {max_value}")
+            out[f"{field}.{name}"] = normalized_data
+    return out, max_value
+
+
+def read_audio(filename):
+    return load_wav_file(get_resources_path(filename))
 
 
 def convert_to_db(ratio_data):
@@ -64,6 +92,6 @@ def db_amp_change(data, db_change):
     return np.sign(data) * convert_to_ratio(db_data)
 
 
-def write_audio(sampling_frequency, data, max_value, file_name):
+def write_audio(sampling_frequency, data, max_value, filename):
     denormalized_data = denormalize_data(data, max_value)
-    write(get_output_resources_path(file_name), sampling_frequency, denormalized_data)
+    write(get_output_resources_path(filename), sampling_frequency, denormalized_data)
--- libs/path.py
+++ libs/path.py
@@ -1,13 +1,28 @@
 from pathlib import Path
 
 
+def get_root_path():
+    curr_path = Path(__file__).parent
+    while curr_path.parent.exists() and not curr_path.joinpath('tasks').is_dir():
+        curr_path = curr_path.parent
+    return curr_path
+
+
 def get_figures_path(*args):
-    return Path('figures', *args)
+    return Path(get_root_path(), 'figures', *args)
 
 
 def get_resources_path(*args):
-    return Path('resources', *args)
+    return Path(get_root_path(), 'resources', *args)
 
 
 def get_output_resources_path(*args):
-    return Path('output_resources', *args)
+    return Path(get_root_path(), 'output_resources', *args)
+
+
+def get_filename_without_ext(filename):
+    return filename[:filename.rindex('.')]
+
+
+def get_ext(filename):
+    return filename[filename.rindex('.') + 1:]
